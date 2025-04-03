c139b24b632654736f39cda4642dc3e9a07a8a26
Muhammad Abuzar <syed.abuzar.badar@gmail.com>
2025-04-03 13:26:30 +0400

    hero section added

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Hero from "./Hero/Hero";
+import Destination from "./Destination/Destination";
 
 const Home = () => {
-  return <div className="overflow-hidden h-[10000px]">Home</div>;
+  return (
+    <div className="overflow-hidden">
+      <Hero />
+      <Destination />
+    </div>
+  );
 };
 
 export default Home;
