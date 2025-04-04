1085bbc7948b888d2057b0fbc6994b378665da90
Muhammad Abuzar <syed.abuzar.badar@gmail.com>
2025-04-04 12:16:12 +0400

    hero video rm

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -30,7 +30,7 @@ const Hero = () => {
             className="rounded px-14 md:px-28 -mt-4 py-2.5 overflow-hidden group bg-rose-600 relative hover:bg-gradient-to-r hover:from-red-500 hover:to-red-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-red-400 transition-all ease-out duration-300"
           >
             <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
-            <span className="relative font-bold">Search</span>
+            <span className="relative font-bold">Search </span>
           </Link>
         </div>
       </div>
