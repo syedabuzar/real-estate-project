2fe3010e63d82c6f0f8dc9e86c32c1442044ba1d
Muhammad Abuzar <syed.abuzar.badar@gmail.com>
2025-04-07 12:18:57 +0400

    added video

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <div className="relative w-full h-[120vh] sm:h-[100vh]">
       <div className="absolute top-0 left-0 w-full h-full bg-gray-800 opacity-70"></div>
       <video
-        src="/images/hero1.jpg"
+        src="/images/hero1.mp4"
         autoPlay
         muted
         loop
